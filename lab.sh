a4c77f9619b33ae864f731d28d834b00bea9d608
siddhu1128 <siddhartha.dasari11@gmail.com>
2021-04-04 10:46:55 +0000

    basic script

---
+++ lab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt update
+sudo apt install docker-ce -y
